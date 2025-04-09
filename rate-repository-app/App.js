1fbbd35f892d08345527e790f233db3d0a507b95
suyash bhardwaj <suyash_bhardwaj@rediffmail.com>
2025-04-09 12:59:42 +0530

    done exercise 10.12 added to the repository

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -2,9 +2,11 @@ import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 const apolloClient = createApolloClient();
 const App = () => {
+  console.log("The expo config is ", Constants.expoConfig);
   return (
     <>
       <NativeRouter>
